da0e89fd97a5ea947b84392ea99f7c76df4d9892
karahiyo <a.ryuklnm@gmail.com>
2013-02-19 04:06:21 +0900

    **

--- lib/lifegame.rb
+++ lib/lifegame.rb
@@ -2,18 +2,78 @@
 # -*- coding:utf-8 -*-
 
 class LifeGame
-  attr_accessor :map_x_length, :map_y_length
+  attr_reader :x, :y
+  attr_accessor :map
 
-  # 生きているセルの配列
-  class ArriveList
+  # マップを初期化
+  def initialize(map)
+    @map = map
+    @x = map[0].size - 1
+    @y = map.size - 1
   end
 
   # 周辺の生きているノード数を返す
-  def surround
+  def surround(x,y)
+    arrive_num = 0
+    (x-1 .. x+1).each do |n|
+      (y-1 .. y+1).each do |m|
+        if n < 0 || m < 0  || 
+          n > @x || m > @y || 
+          n==x && m==y
+          next
+        end
+        arrive_num += 1 if @map[n][m] == 1
+      end
+    end
+    arrive_num
+  end
+
+  # [誕生]
+  def willbeBorn(n,m)
+    life = 0
+    life = 1 if surround(n,m) == 3
+    life
+  end
+
+  # [生存]
+  def servive(n,m)
+    life = 1
+    a = surround(n,m)
+    life = 0 unless a == 2 || a == 3
+    life
+  end
+
+  # [過疎] & [過密]
+  def deador(n,m)
+    life = 1
+    a = surround(n,m)
+    life = 0 if a <= 1 || a >= 4
   end
 
   # 次世代のArriveListを返す
   def next
+    @x.times do |n|
+      @y.times do |m|
+        case map[n][m]
+        when 0 
+          map[n][m] =  willbeBorn(n,m)
+        when 1
+          map[n][m] = deador(n,m)
+        end
+      end
+    end
+
+    map
   end
     
+  def display
+    puts "\n*******************"
+    @x.times do |n|
+      @y.times do |m|
+        printf "#{map[n][m]} "
+      end
+      printf "\n"
+    end
+    printf "*******************\n"
+  end
 end
--- test/lifegame_test.rb
+++ test/lifegame_test.rb
@@ -3,7 +3,50 @@
 
 require 'test/unit'
 require './lib/lifegame'
-#require File.join(File.dirname(__FILE__), '../lib/lifegame')
+#require File.join(File.dirname(__FILE__), './../lib/lifegame')
 
 class LifeGameTest < Test::Unit::TestCase
+
+  def test_surround
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,0]])
+    a = lg.surround(1,1)
+    assert_equal 3, a
+    b = lg.surround(0,0)
+    assert_equal 2, b
+    c = lg.surround(2,2)
+    assert_equal 0, c
+  end
+
+  def test_willbeBorn_1
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,0]])
+    a = lg.willbeBorn(1,1)
+    assert_equal 1, a
+  end
+
+  def test_willbeBorn_2
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,0]])
+    b = lg.willbeBorn(0,2)
+    assert_equal 0, b
+  end
+
+  def test_willbeBorn_3
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,0]])
+    c = lg.willbeBorn(2,2)
+    assert_equal 0, c
+  end
+
+  def test_deador
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,1]])
+    a = lg.deador(2,2)
+    assert_equal 0, a
+  end
+
+  def test_next
+    lg = LifeGame.new([[1,1,0],[1,0,0],[0,0,0]])
+    lg.display
+    lg.next
+    lg.display
+  end
+
 end
+
