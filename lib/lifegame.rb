817c37bd9ab2634bb438b00317669d9f98e5187e
karahiyo <a.ryuklnm@gmail.com>
2013-02-19 01:20:27 +0900

    [lifegame] #vg

---
+++ lib/lifegame.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby
+# -*- coding:utf-8 -*-
+
+class LifeGame
+  attr_accessor :map_x_length, :map_y_length
+
+  # 生きているセルの配列
+  class ArriveList
+  end
+
+  # 周辺の生きているノード数を返す
+  def surround
+  end
+
+  # 次世代のArriveListを返す
+  def next
+  end
+    
+end
---
+++ test/lifegame_test.rb
@@ -0,0 +1,9 @@
+#!/usr/local/bin/ruby
+# -*- coding:utf-8 -*-
+
+require 'test/unit'
+require './lib/lifegame'
+#require File.join(File.dirname(__FILE__), '../lib/lifegame')
+
+class LifeGameTest < Test::Unit::TestCase
+end
